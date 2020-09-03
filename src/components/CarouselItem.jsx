234f5c40f5da74ac343ac31fd60447e8ed05bb29
Angel <angel_cast@outlook.es>
2020-09-03 09:31:51 -0500

    Añadiendo imagenes con Webpack

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import "../assets/styles/components/CarouselItem.scss";
+import playIcon from "../assets/static/icons8-play-64.png";
+import plusIcon from "../assets/static/icons8-plus-64.png";
 
 const CarouselItem = () => (
     <div className="carousel-item">
         <img className="carousel-item__img" src="https://images.pexels.com/photos/5202030/pexels-photo-5202030.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="1" />
         <div className="carousel-item__details">
             <div>
-                <img className="carousel-item__details--icon" src="assets/icons8-play-64.png" alt="Play" />
-                <img className="carousel-item__details--icon" src="assets/icons8-plus-64.png" alt="Plus" />
+                <img className="carousel-item__details--img" src={playIcon} alt="Play" />
+                <img className="carousel-item__details--img" src={plusIcon} alt="Plus" />
             </div>
             <p className="carousel-item__details--title">Título descriptivo</p>
             <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -12,7 +12,7 @@ const App = () => (
     <div className="App">
         <Header />
         <Search />
-        <Categories>
+        <Categories title="Mi Lista">
             <Carousel>
                 <CarouselItem />
                 <CarouselItem />
@@ -20,6 +20,21 @@ const App = () => (
                 <CarouselItem />
             </Carousel>
         </Categories>
+
+        <Categories title="Tendencias">
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+
+        <Categories title="Originales de Platzi Video">
+            <Carousel>
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+
         <Footer />
     </div>
 );
